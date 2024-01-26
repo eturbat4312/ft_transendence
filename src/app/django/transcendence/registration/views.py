319923b241044cfa2efe3160afa7ff61dea0afe4
tory4312 <86555871+tory4312@users.noreply.github.com>
2024-01-26 10:16:09 +0100

    backend testing

--- src/app/django/transcendence/registration/views.py
+++ src/app/django/transcendence/registration/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
-def login(request):
-    return render(request, 'login.html')
+# from rest_framework.contrib.authtoken.models import Token
+
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+
+
+@api_view(["POST"])
 def register(request):
-    return render(request, 'register.html')
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data["username"])
+        user.set_password(request.data["password"])
+        user.save()
+        return Response({"user": serializer.data})
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
