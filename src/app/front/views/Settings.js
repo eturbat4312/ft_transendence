1358fd43627fbe40db0b3aba62fdcef96e4ab09b
Blaise <blaze.kalonda@gmail.com>
2024-04-01 15:59:00 +0200

    profilePage

--- src/app/front/views/Settings.js
+++ src/app/front/views/Settings.js
@@ -4,9 +4,14 @@ export default class Settings extends AbstractView {
     constructor(params) {
         super(params);
         this.setTitle("Settings");
+        //this.setInfo().then(r => console.log('Settings Info loaded'))
     }
 
     async getHtml() {
+        const profileSection = await this.profileSection();
+        const changePasswordSection = this.changePasswordSection();
+        const bioSection = this.bioSection();
+
         return `
         <div class="container mt-3 centered">
             <div class="card overflow-hidden mx-auto bg-dark text-light" style="max-width: 800px;">
@@ -23,84 +28,154 @@ export default class Settings extends AbstractView {
                     </div>
                     <div class="col-md-9">
                         <div class="tab-content" style="height: 500px; overflow-y: auto;">
-                            <div class="tab-pane fade active show" id="account-general">
-                                <div class="card-body media align-items-center">
-                                <img src="https://m.media-amazon.com/images/I/51G2Jr9BjOL._AC_UF1000,1000_QL80_.jpg" alt="Photo de profil" class="img-fluid rounded-circle" style="width: 150px; height: 150px;"> 
-                                    <div class="media-body ml-4">
-                                        <label class="btn btn-outline-primary">
-                                            Upload new photo
-                                            <input type="file" class="account-settings-fileinput">
-                                        </label> &nbsp;
-                                        <button type="button" class="btn btn-default md-btn-flat">Reset</button>
-                                    </div>
-                                </div>
-                                <hr class="border-light m-0">
-                                <div class="card-body">
-                                    <div class="form-group">
-                                        <label class="form-label">Username</label>
-                                        <input type="text" class="form-control mb-1" value="Qwerty42">
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="form-label">Name</label>
-                                        <input type="text" class="form-control" value="john smith">
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="form-label">E-mail</label>
-                                        <input type="text" class="form-control mb-1" value="qwerty42@mail.com">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="tab-pane fade" id="account-change-password">
-                                <div class="card-body pb-2">
-                                    <div class="form-group">
-                                        <label class="form-label">Current password</label>
-                                        <input type="password" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="form-label">New password</label>
-                                        <input type="password" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="form-label">Repeat new password</label>
-                                        <input type="password" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="tab-pane fade" id="account-info">
-                                <div class="card-body pb-2">
-                                    <div class="form-group">
-                                        <label class="form-label">Bio</label>
-                                        <textarea class="form-control"
-                                            rows="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nunc arcu, dignissim sit amet sollicitudin iaculis, vehicula id urna. Sed luctus urna nunc. Donec fermentum, magna sit amet rutrum pretium, turpis dolor molestie diam, ut lacinia diam risus eleifend sapien. Curabitur ac nibh nulla. Maecenas nec augue placerat, viverra tellus non, pulvinar risus.</textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="form-label">Birthday</label>
-                                        <input type="text" class="form-control" value="">
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="form-label">Country</label>
-                                        <select class="custom-select">
-                                            <option>USA</option>
-                                            <option selected>Switzerland</option>
-                                            <option>France</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <hr class="border-light m-0">
-                                <div class="card-body pb-2">
-                                    <h6 class="mb-4">Contacts</h6>
-                                    <div class="form-group">
-                                        <label class="form-label">Phone</label>
-                                        <input type="text" class="form-control" value="">
-                                    </div>
-                                </div>
-                            </div>
+                            ${profileSection}
+                            ${changePasswordSection}
+                            ${bioSection}
                         </div>
                     </div>
                 </div>
-                <button type="button" style="bottom: 0; left: 0;" class="btn btn-primary">Save changes</button>&nbsp;
             </div>
         </div>
         `;
+    }
+
+    async profileSection() {
+        const profilePicUrl = await this.getProfilePicUrl();
+        const userName = await this.getUsername();
+        return `
+        <div class="tab-pane fade active show" id="account-general">
+            <div class="card-body media align-items-center">
+                <img id="profilePic" src="${profilePicUrl}" alt="Photo de profil" class="img-fluid rounded-circle" style="width: 150px; height: 150px;"> 
+                <div class="media-body ml-4">
+                    <label class="btn btn-outline-primary">
+                        Upload new photo
+                        <input type="file" class="account-settings-fileinput">
+                    </label> &nbsp;
+                </div>
+            </div>
+            <hr class="border-light m-0">
+            <div class="card-body">
+                <div class="form-group">
+                    <span id="username"></span>
+                </div>
+                 <div class="form-group">
+                    <label class="form-label">Nickname</label>
+                    <input id="Nickname" type="text" class="form-control mb-1" value="">
+                </div>
+                <div class="form-group">
+                    <label class="form-label">E-mail</label>
+                    <input id="email" type="text" class="form-control mb-1" value="">
+                </div>
+            </div>
+            <button type="button" style="bottom: 0; left: 0;" class="btn btn-primary">Save changes</button>&nbsp;
+        </div>
+        `;
+    }
+
+    changePasswordSection() {
+        return `
+    <div class="tab-pane fade" id="account-change-password">
+        <div class="card-body pb-2">
+            <div class="form-group">
+                <label class="form-label">Current password</label>
+                <input id="currentPasswordInput" type="password" class="form-control">
+            </div>
+            <div class="form-group">
+                <label class="form-label">New password</label>
+                <input id="newPasswordInput" type="password" class="form-control">
+            </div>
+            <div class="form-group">
+                <label class="form-label">Repeat new password</label>
+                <input id="repeatPasswordInput" type="password" class="form-control">
+            </div>
+            <button id="changePasswordButton" type="button" class="btn btn-primary">Change Password</button>
+            <div id="passwordChangeMessage"></div>
+        </div>
+    </div>
+    `;
+    }
+
+    bioSection() {
+        return `
+        <div class="tab-pane fade" id="account-info">
+            <div class="card-body pb-2">
+                <div class="form-group">
+                    <label class="form-label">Bio</label>
+                    <textarea class="form-control" rows="5"></textarea>
+                </div>
+            </div> 
+            <button type="button" style="bottom: 0; left: 0;" class="btn btn-primary">Save changes</button>&nbsp;
+        </div>
+        `;
+    }
+
+    async setInfo() {
+        const serverIP = window.location.hostname;
+        const token = localStorage.getItem('token');
+        if (!token) {
+            console.log('Token not found');
+            return;
+        }
+
+
+    }
+
+    async getProfilePicUrl() {
+        const serverIP = window.location.hostname;
+        const token = localStorage.getItem('token');
+        if (!token) {
+            console.log('Token not found');
+            return;
+        }
+
+        try {
+            const response = await fetch(`https://${serverIP}/api/get_profile_pic/`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Token ' + token
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                return data.profile_pic_url;
+            } else {
+                console.log('Failed to get profile pic:', await response.text());
+                return null;
+            }
+
+        } catch (error) {
+            console.log('Error:', error);
+            return null;
+        }
+    }
+
+    async getUsername ()    {
+        const serverIP = window.location.hostname;
+        const token = localStorage.getItem('token');
+        if (!token) {
+            console.log('Token not found');
+            return;
+        }
+
+        try {
+            const responseUsername = await fetch(`https://${serverIP}/api/get_username/`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Token ' + token
+                }
+            });
+
+            if (responseUsername.ok) {
+                const userData = await responseUsername.json();
+                const username = userData.username;
+                document.getElementById('username').innerText = 'Username: ' + username;
+            } else {
+                console.log('Failed to get username:', await responseUsername.text());
+            }
+
+        } catch (error) {
+            console.log('Error:', error);
+        }
     }
 }
